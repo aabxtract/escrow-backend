ef62287a65dd7e885cb084d1f9a1e46b4669c3eb
Douglas Francis <douglasfrancis054@gmail.com>
2026-06-13 13:38:06 +0100

    fix: add type: module and .js import extension

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import jobRoutes from "./routes/jobs";
+import jobRoutes from "./routes/jobs.js";
 
 dotenv.config();
 
